ad7360226571a9cbaad564438fca7910e5041461
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-22 19:50:10 +0900

    Add report title

--- day-report.rb
+++ day-report.rb
@@ -16,27 +16,27 @@ events.each do |_|
   case _.type
   # commit
   when "PushEvent"
-    url_to_detail[_.id] ||= {title: "push to #{_.repo.name}", commits: _.payload.commits, repo: _.repo, date: _.created_at.getlocal}
+    url_to_detail[_.id] ||= {title: "Push to #{_.repo.name}", commits: _.payload.commits, repo: _.repo, date: _.created_at.getlocal}
   # Wiki
   when "GollumEvent"
-    url_to_detail[_.id] ||= {title: "edit wiki of #{_.repo.name}", pages: _.payload.pages, repo: _.repo, date: _.created_at.getlocal}
+    url_to_detail[_.id] ||= {title: "Edit wiki of #{_.repo.name}", pages: _.payload.pages, repo: _.repo, date: _.created_at.getlocal}
   # Issue
   when "IssuesEvent"
-    url_to_detail[_.payload.issue.html_url] ||= {title: _.payload.issue.title, comments: [], repo: _.repo, date: _.created_at.getlocal}
+    url_to_detail[_.payload.issue.html_url] ||= {title: "Issue about '#{_.payload.issue.title}'", comments: [], repo: _.repo, date: _.created_at.getlocal}
   # Issue comments
   when "IssueCommentEvent"
-    url_to_detail[_.payload.issue.html_url] ||= {title: _.payload.issue.title, comments: [], repo: _.repo, date: _.created_at.getlocal}
+    url_to_detail[_.payload.issue.html_url] ||= {title: "Comment about '#{_.payload.issue.title}'", comments: [], repo: _.repo, date: _.created_at.getlocal}
     url_to_detail[_.payload.issue.html_url][:comments] << _.payload.comment.html_url
   # pull-request
   when "PullRequestEvent"
-    url_to_detail[_.payload.pull_request.html_url] ||= {title: _.payload.pull_request.title, comments: [], repo: _.repo, date: _.created_at.getlocal}
+    url_to_detail[_.payload.pull_request.html_url] ||= {title: "Pull-request about '#{_.payload.pull_request.title}'", comments: [], repo: _.repo, date: _.created_at.getlocal}
   end
 end
 
 # create day report
 puts Kramdown::Document.new("## #{Time.now.to_date} Day Report of #{user}").to_html
 url_to_detail.each do |url, detail|
-  # p detail
+  #p detail
   puts Kramdown::Document.new("##### #{detail[:date]} : #{detail[:title]}").to_html
   unless detail[:comments].nil?
     detail[:comments].reverse.each do |comment|
